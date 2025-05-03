c8797f4f7733632f79b4dbc51a06df9a25093997
kroko <krokon199@gmail.com>
2025-05-03 10:50:39 +0600

    new struct

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
       <BrowserRouter>
           <Routes>
+              <Route>
 
+              </Route>
           </Routes>
       </BrowserRouter>
   )
